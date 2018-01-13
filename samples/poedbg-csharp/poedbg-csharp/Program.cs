d67fb51e3a050ca8a6630e0430803bfa3e7e6fd9
maper <m4p3r@outlook.com>
2018-01-13 00:56:37 -0800

    Added initial binary and samples.

---
+++ samples/poedbg-csharp/poedbg-csharp/Program.cs
@@ -0,0 +1,105 @@
+﻿// Part of 'poedbg'. Copyright (c) 2018 maper. Copies must retain this attribution.
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace poedbg_csharp
+{
+    class Program
+    {
+        // First we need to define types for the callbacks that we're going
+        // to register with poedbg.
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        public delegate void PoeDbgErrorCallback(int ErrorCode);
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        public delegate void PoeDbgPacketSendCallback(int Length, byte Id, byte[] Data);
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        public delegate void PoeDbgPacketReceiveCallback(int Length, byte Id, byte[] Data);
+
+        // Next we need to reference all of the functions we will import
+        // from poedbg.
+
+        [DllImport("poedbg.dll")]
+        public static extern int PoeDbgInitialize();
+
+        [DllImport("poedbg.dll")]
+        public static extern int PoeDbgRegisterErrorCallback([MarshalAs(UnmanagedType.FunctionPtr)] PoeDbgErrorCallback Callback);
+
+        [DllImport("poedbg.dll")]
+        public static extern int PoeDbgRegisterPacketSendCallback([MarshalAs(UnmanagedType.FunctionPtr)] PoeDbgPacketSendCallback Callback);
+
+        [DllImport("poedbg.dll")]
+        public static extern int PoeDbgRegisterPacketReceiveCallback([MarshalAs(UnmanagedType.FunctionPtr)] PoeDbgPacketReceiveCallback Callback);
+
+        [DllImport("poedbg.dll")]
+        public static extern int PoeDbgUnregisterErrorCallback();
+
+        [DllImport("poedbg.dll")]
+        public static extern int PoeDbgUnregisterPacketSendCallback();
+
+        [DllImport("poedbg.dll")]
+        public static extern int PoeDbgUnregisterPacketReceiveCallback();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting 'poedbg' C# sample...");
+
+            // We'll define the callback handlers here as anonymous functions just
+            // for the sake of example.
+
+            PoeDbgErrorCallback ErrorCallback =
+            (ErrorCode) =>
+            {
+                Console.WriteLine("[ERROR] The 'poedbg' module reported an error code of '{0}'.", ErrorCode);
+            };
+
+            PoeDbgPacketSendCallback SendCallback =
+            (Length, Id, Data) =>
+            {
+                Console.WriteLine("[SENT] Packet with ID of '{0}' and length of '{1}'.", Id, Length);
+            };
+
+            PoeDbgPacketReceiveCallback ReceiveCallback =
+            (Length, Id, Data) =>
+            {
+                Console.WriteLine("[RECEIVED] Packet with ID of '{0}' and length of '{1}'.", Id, Length);
+            };
+
+            // Now we'll register all of our callbacks. It is important, especially,
+            // that we register the error handling callback before we attempt to
+            // initialize the poedbg module.
+
+            int Status = PoeDbgRegisterErrorCallback(ErrorCallback);
+            if (Status < 0)
+            {
+                Console.WriteLine("[ERROR] Could not register error callback. Error code: '{0}'.", Status);
+            }
+
+            Status = PoeDbgRegisterPacketSendCallback(SendCallback);
+            if (Status < 0)
+            {
+                Console.WriteLine("[ERROR] Could not register packet send callback. Error code: '{0}'.", Status);
+            }
+
+            Status = PoeDbgRegisterPacketReceiveCallback(ReceiveCallback);
+            if (Status < 0)
+            {
+                Console.WriteLine("[ERROR] Could not register packet receive callback. Error code: '{0}'.", Status);
+            }
+
+            Status = PoeDbgInitialize();
+            if (Status < 0)
+            {
+                Console.WriteLine("[ERROR] Could not initialize. Error code: '{0}'.", Status);
+            }
+
+            Console.WriteLine("Started successfully.");
+
+            // Wait.
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ samples/poedbg-python/poedbg-python.py
@@ -0,0 +1,57 @@
+# Part of 'poedbg'. Copyright (c) 2018 maper. Copies must retain this attribution.
+
+import ctypes
+import time
+import sys
+
+# First, we need to specify the types of our callbacks so that the ctypes
+# module knows what to do with them when we pass them to poedbg.
+
+@ctypes.WINFUNCTYPE(None, ctypes.c_int)
+def poedbg_error_callback(error_code):
+    print("[ERROR] The 'poedbg' module reported an error code of '{}'.".format(error_code))
+
+@ctypes.WINFUNCTYPE(None, ctypes.c_int, ctypes.c_byte, ctypes.c_char_p)
+def poedbg_packet_receive_callback(packet_length, packet_id, packet_data):
+    print("[RECEIVED] Packet with ID of '{}' and length of '{}'.".format(packet_id, packet_length))
+
+@ctypes.WINFUNCTYPE(None, ctypes.c_int, ctypes.c_byte, ctypes.c_char_p)
+def poedbg_packet_send_callback(packet_length, packet_id, packet_data):
+    print("[SENT] Packet with ID of '{}' and length of '{}'.".format(packet_id, packet_length))
+
+
+def main():
+    print("Starting 'poedbg' Python sample...")
+
+    # Load an instance of poedbg.
+    poedbg_dll = ctypes.windll.LoadLibrary('poedbg.dll')
+
+    # Next, we can directly call all of the poedbg functions to set up
+    # our callbacks. We should make sure that we set up the error handler
+    # especially before initializing the module later.
+
+    poedbg_status = poedbg_dll.PoeDbgRegisterErrorCallback(poedbg_error_callback)
+    if poedbg_status < 0:
+        print("[ERROR] Could not register error callback. Error code: '{}'.".format(poedbg_status))
+
+    poedbg_status = poedbg_dll.PoeDbgRegisterPacketReceiveCallback(poedbg_packet_receive_callback)
+    if poedbg_status < 0:
+        print("[ERROR] Could not register packet receive callback. Error code: '{}'.".format(poedbg_status))
+
+    poedbg_status = poedbg_dll.PoeDbgRegisterPacketSendCallback(poedbg_packet_send_callback)
+    if poedbg_status < 0:
+        print("[ERROR] Could not register packet send callback. Error code: '{}'.".format(poedbg_status))
+
+    poedbg_status = poedbg_dll.PoeDbgInitialize()
+    if poedbg_status < 0:
+        print("[ERROR] Could not intitialize. Error code: '{0}'.".format(poedbg_status))
+
+    print("Started successfully.")
+
+    # If we let the main thread die, our game hooks will be un-set. This
+    # will crash the game.
+
+    raw_input("(Pressing a key will end the program)...")
+
+if __name__ == "__main__":
+    main()
